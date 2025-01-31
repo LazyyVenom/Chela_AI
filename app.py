db2fa4940af5ddbfe19b5dec6e49b1283c9d3a8f
Anubhav Choubey <choubey.anubhav253@gmail.com>
2025-01-31 19:41:04 +0530

    Planning done need to execute

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
 # I will start working in here to create the app
+
+# Try Adding speech recognition
+# Starting with this
+
+# Try defining for classification
+
+# Try to add functionalities accordingly
+
