36f39c57a03b3c077d6ebef96d5c5dd2b4fee40b
Anubhav Choubey <choubey.anubhav253@gmail.com>
2025-02-02 16:02:20 +0530

    UI looking good

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import os
 import eel
+from backend_features.sound import playAssistantSound
 
 eel.init('www')
 os.system('start msedge.exe --app="http://localhost:8000/index.html"')
 
+playAssistantSound()
 eel.start('index.html',mode=None)
---
+++ backend_features/sound.py
@@ -0,0 +1,4 @@
+from playsound import playsound
+
+def playAssistantSound():
+    playsound("www/assets/sound.mp3")
