8b3bfb4dc6b74f408d43a8f0411015f12c894f1e
Anubhav Choubey <choubey.anubhav253@gmail.com>
2025-02-04 15:50:00 +0530

    Adding functioning first then UI

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import eel
 import os
 from backend_features.sound import playAssistantSound
 from assistant_functions.speech_reco import SpeechRecognizer
@@ -8,8 +9,10 @@ def listen_for_wake_word():
     recognizer.listen_for_wake_word()
 
 if __name__ == "__main__":
+    eel.init("www")
     os.system('start msedge.exe --app="http://localhost:8000/index.html"')
     playAssistantSound()
     p = mp.Process(target=listen_for_wake_word)
     p.start()
     print("STARTING EEL APP")
+    eel.start("index.html",mode=None)
